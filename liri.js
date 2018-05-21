67a397618f73ec2e5c596f67ed6c5ec9107d4ec5
Anjie <anjelinamoore@gmail.com>
2018-05-21 16:48:07 -0600

    Twitter somewhat working

--- liri.js
+++ liri.js
@@ -1,4 +1,19 @@
 require("dotenv").config();
 
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var keys = require('./keys');
+
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+var params = {screen_name: 'anjiederp'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+      return(error);
+  } else { (process.argv[2] === "my-tweets") 
+    for (var i = 0; i < tweets.length; i++)
+    console.log(tweets[i].text);
+  }
+});
+
